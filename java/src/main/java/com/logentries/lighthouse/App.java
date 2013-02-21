0b398efd8cb2216d39d460fcb3cc6ecb06608eab
Daniel Fiala <danfiala@ucw.cz>
2013-02-21 09:59:54 +0100

    Added the backbone of the Java client.

---
+++ java/src/main/java/com/logentries/lighthouse/App.java
@@ -0,0 +1,13 @@
+package com.logentries.lighthouse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
