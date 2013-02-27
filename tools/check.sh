68b8c1ad779aa6b568330d27ce75fda825ed3e77
Daniel Fiala <danfiala@ucw.cz>
2013-02-27 09:53:53 +0100

    Renamed check.py, because it is bash scipt, not python script. :-)

--- tools/check.sh
+++ tools/check.sh
@@ -6,6 +6,7 @@ pychecker --limit 20 tests
 pychecker --limit 20 lighthouse
 
 cd lighthouse
+pychecker --limit 20 sync
 pychecker --limit 20 data
 pychecker --limit 20 server
 pychecker --limit 20 main
